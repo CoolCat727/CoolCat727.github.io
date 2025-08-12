8afb61ad2c1b33f2757f21883dc07a34cb4ae4cc
leochen <leochen@acube.com.tw>
2025-08-12 13:52:20 +0800

    client

--- coolcat-client/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-import type { components } from '@/types/api';
-
-type schemas = components['schemas']
-
-// users
-export type UserEntity = schemas['UserEntity'];
-export type CreateUserDto = schemas['CreateUserDto'];
-export type UpdateUserDto = schemas['UpdateUserDto'];
